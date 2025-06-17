d6e452545ae390034bed3d4818a4fe16da9b650e
Simon Myhill <myhillsimon96@gmail.com>
2025-06-17 11:40:42 -0400

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <HelloWorld />
+</template>
+
+<style scoped>
+/* You can add global styles here if needed */
+</style>
